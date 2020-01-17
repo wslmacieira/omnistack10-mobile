a5c49ea8636bc5347ac3eb8481edce5406a188db
wslmacieira <wslmacieira@DESKTOP-GJDMPV4.localdomain>
2020-01-17 13:04:14 -0300

    aula 05 Funcionalidades avançadas

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,6 +5,7 @@ import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 import { MaterialIcons } from '@expo/vector-icons';
 
 import api from '../services/api';
+import { connect, disconnect, subscribeToNewDevs } from '../services/socket';
 
 function Main({ navigation }) {
   const [devs, setDevs] = useState([]);
@@ -34,6 +35,22 @@ function Main({ navigation }) {
     loadInitialPosition();
   }, []);
 
+  useEffect(() => {
+    subscribeToNewDevs(dev => setDevs([...devs, dev]))
+  }, [devs])
+
+  function setupWebsocket() {
+    const { latitude, longitude } = currentRegion;
+
+    disconnect();
+
+    connect(
+      latitude,
+      longitude,
+      techs,
+    );
+  }
+
   async function loadDevs() {
     const { latitude, longitude } = currentRegion
 
@@ -45,7 +62,9 @@ function Main({ navigation }) {
       }
     });
 
+
     setDevs(response.data.devs);
+    setupWebsocket();
   }
 
   function handleRegionComplete(region) {
